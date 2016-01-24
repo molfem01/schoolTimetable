ccdfb789352cd8dbb9d1b74f0e8ec83d1d73cb3e
marcooocram <marco@molfese.de>
2016-01-24 21:04:49 +0100

    tt001 - initial setup of environment

---
+++ timetableprogramm/src/main/java/control/TimeTableFactory.java
@@ -0,0 +1,21 @@
+package control;
+
+import model.TimetableDO;
+import model.TimetableSpecsDO;
+
+/**
+ * Created by Marco on 24/01/2016.
+ */
+public class TimeTableFactory {
+
+    public TimetableDO getEmptyTimeTable(TimetableSpecsDO specs){
+
+        TimetableDO timetable = new TimetableDO();
+
+
+
+        return timetable;
+    }
+
+
+}
---
+++ timetableprogramm/src/main/java/model/TimetableSpecsDO.java
@@ -0,0 +1,8 @@
+package model;
+
+/**
+ * Created by Marco on 24/01/2016.
+ */
+public class TimetableSpecsDO {
+
+}
